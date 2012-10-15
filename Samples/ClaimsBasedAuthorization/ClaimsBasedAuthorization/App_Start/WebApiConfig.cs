325980558122c0ee8992f8b618b79a6faf5f3fc5
Dominick Baier <dbaier@leastprivilege.com>
2012-10-15 17:46:50 +0200

    Added "Claims-based Authorization" sample (base)

---
+++ Samples/ClaimsBasedAuthorization/ClaimsBasedAuthorization/App_Start/WebApiConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Thinktecture.IdentityModel.Authorization.WebApi;
+
+namespace ClaimsBasedAuthorization
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // add global authorization filter
+            config.Filters.Add(new ApiClaimsAuthorizeAttribute());
+        }
+    }
+}
---
+++ Samples/ClaimsBasedAuthorization/ClaimsBasedAuthorization/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClaimsBasedAuthorization.MvcApplication" Language="C#" %>
