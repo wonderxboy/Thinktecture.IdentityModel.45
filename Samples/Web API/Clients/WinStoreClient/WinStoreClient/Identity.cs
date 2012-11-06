2472c88d36124fd8f5b6cb8c0f0a1dbbebf3de8b
Dominick Baier <dbaier@leastprivilege.com>
2012-11-06 04:04:52 -0800

    Added Windows Store Client sample

---
+++ Samples/Web API/Clients/WinStoreClient/WinStoreClient/Identity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinStoreClient
+{
+    public class Identity
+    {
+        public string Name { get; set; }
+        public string AuthenticationType { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public string ClrType { get; set; }
+
+        public ClaimsDto Claims { get; set; }
+
+
+    }
+
+    public class ClaimsDto : List<ClaimDto>
+    {
+        public ClaimsDto()
+        { }
+
+        public ClaimsDto(IEnumerable<ClaimDto> claims)
+            : base(claims)
+        { }
+    }
+
+    public class ClaimDto
+    {
+        public string ClaimType { get; set; }
+        public string Value { get; set; }
+        public string Issuer { get; set; }
+        public string OriginalIssuer { get; set; }
+    }
+}
