d09dfe2d454d788a5d2271d86ded491019fc0acf
Dominick Baier <dbaier@leastprivilege.com>
2012-11-08 09:13:02 +0100

    Added plumbing for HttpRequestMessage based authentication

---
+++ IdentityModel/Thinktecture.IdentityModel/Tokens/Http/HttpRequestSecurityToken.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+using System.Net.Http;
+
+namespace Thinktecture.IdentityModel.Tokens.Http
+{
+    public class HttpRequestSecurityToken : SecurityToken
+    {
+        HttpRequestMessage _request;
+
+        public HttpRequestMessage Request 
+        {
+            get
+            {
+                return _request;
+            }
+        }
+
+        public HttpRequestSecurityToken(HttpRequestMessage request)
+        {
+            _request = request;
+        }
+
+        #region Not Implemented
+        public override string Id
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override System.Collections.ObjectModel.ReadOnlyCollection<SecurityKey> SecurityKeys
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override DateTime ValidFrom
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override DateTime ValidTo
+        {
+            get { throw new NotImplementedException(); }
+        }
+        #endregion
+    }
+}
---
+++ IdentityModel/Thinktecture.IdentityModel/Tokens/Http/HttpRequestSecurityTokenHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IdentityModel.Tokens;
+using System.Net.Http;
+using System.Security.Claims;
+
+namespace Thinktecture.IdentityModel.Tokens.Http
+{
+    public class HttpRequestSecurityTokenHandler : SecurityTokenHandler
+    {
+        private string[] _identifier;
+        public delegate ClaimsPrincipal ValidateTokenDelegate(HttpRequestMessage request);
+
+        public ValidateTokenDelegate Validator { get; set; }
+
+        public HttpRequestSecurityTokenHandler(string identifier)
+            : this(identifier, null)
+        { }
+
+        public HttpRequestSecurityTokenHandler(ValidateTokenDelegate validator) 
+            : this(Guid.NewGuid().ToString(), validator)
+        { }
+
+        public HttpRequestSecurityTokenHandler(string identifier, ValidateTokenDelegate validator)
+        {
+            _identifier = new string[] { identifier };
+            Validator = validator;
+        }
+
+        public override ReadOnlyCollection<ClaimsIdentity> ValidateToken(SecurityToken token)
+        {
+            var requestToken = token as HttpRequestSecurityToken;
+            if (requestToken == null)
+            {
+                throw new ArgumentException("SecurityToken is not a SimpleSecurityToken");
+            }
+
+            var identity = Validator(requestToken.Request).Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                return new List<ClaimsIdentity> { identity }.AsReadOnly();
+            }
+            else
+            {
+                throw new SecurityTokenValidationException("No identity");
+            }
+        }
+
+        public override string[] GetTokenTypeIdentifiers()
+        {
+            return _identifier;
+        }
+
+        public override Type TokenType
+        {
+            get { return typeof(HttpRequestSecurityToken); }
+        }
+    }
+}
