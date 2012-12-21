ea643306c9bce79ef09dad7daf1e16f1b28e3eb1
Dominick Baier <dbaier@leastprivilege.com>
2012-12-21 12:36:27 +0100

    Added extension method for HttpClient

---
+++ IdentityModel/Thinktecture.IdentityModel/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Thinktecture.IdentityModel.Tokens.Http;
+
+namespace System.Net.Http
+{
+    public static class HttpClientExtensions
+    {
+        public static void SetBasicAuthentication(this HttpClient client, string userName, string password)
+        {
+            client.DefaultRequestHeaders.Authorization = new BasicAuthenticationHeaderValue(userName, password);
+        }
+    }
+}
