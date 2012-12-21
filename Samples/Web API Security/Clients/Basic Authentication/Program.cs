635829a1a5894de38476721c22158669fe61c3d7
Dominick Baier <dbaier@leastprivilege.com>
2012-12-21 12:36:41 +0100

    Added Basic Auth sample client

---
+++ Samples/Web API Security/Clients/Basic Authentication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Thinktecture.Samples;
+using Thinktecture.Samples.Data;
+
+namespace BasicAuthenticationClient
+{
+    class Program
+    {
+        static Uri _baseAddress = new Uri(Constants.WebHostBaseAddress);
+        //static Uri _baseAddress = new Uri(Constants.SelfHostBaseAddress);
+
+        static void Main(string[] args)
+        {
+            var client = new HttpClient {
+                BaseAddress = _baseAddress
+            };
+
+            client.SetBasicAuthentication("bob", "bob");
+
+            while (true)
+            {
+                Helper.Timer(() =>
+                {
+                    var response = client.GetAsync("identity").Result;
+                    response.EnsureSuccessStatusCode();
+
+                    var claims = response.Content.ReadAsAsync<IEnumerable<ViewClaim>>().Result;
+                    Helper.ShowConsole(claims);
+                });
+
+                Console.ReadLine();
+            }            
+        }
+    }
+}
---
+++ Samples/Web API Security/Common/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thinktecture.Samples
+{
+    public static class Constants
+    {
+        public const string WebHostName = "roadie";
+        public const string SelfHostName = "roadie";
+        
+        public const string AppName = "/webapisecurity/api/";
+
+        public const string WebHostBaseAddress = "https://" + WebHostName + AppName;
+        public const string SelfHostBaseAddress = "https://" + SelfHostName + AppName;
+    }
+}
