0f1ecc5dff18d41a223c06dbf8a935dc6dedd7c5
Dominick Baier <dbaier@leastprivilege.com>
2012-12-21 14:49:23 +0100

    Updated constants

--- Samples/Web API Security/Common/Constants.cs
+++ Samples/Web API Security/Common/Constants.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Thinktecture.Samples
+﻿namespace Thinktecture.Samples
 {
     public static class Constants
     {
+        //
+        // change the below constants to match your local system
+        //
+
         public const string WebHostName = "roadie";
         public const string SelfHostName = "roadie";
         
-        public const string AppName = "/webapisecurity/api/";
+        public const string WebHostAppName = "/webapisecurity/api/";
+        public const string SelfHostAppName = "/webapisecurity/api/";
 
-        public const string WebHostBaseAddress = "https://" + WebHostName + AppName;
-        public const string SelfHostBaseAddress = "https://" + SelfHostName + AppName;
+        public const string WebHostBaseAddress = "https://" + WebHostName + WebHostAppName;
+        public const string SelfHostBaseAddress = "https://" + SelfHostName + SelfHostAppName;
     }
 }
