228b7ba5f7e4eb6920e1939bb74fc23f92191a10
Dominick Baier <dbaier@leastprivilege.com>
2012-12-21 10:34:21 +0100

    Added basic auth support to sample

---
+++ Samples/Web API Security/Common/Security/WebApiConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Http;
+using Thinktecture.IdentityModel.Tokens.Http;
+
+namespace Thinktecture.Samples.Security
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var authentication = CreateAuthenticationConfiguration();
+
+            // route to identity controller
+            config.Routes.MapHttpRoute(
+                name: "Identity",
+                routeTemplate: "api/identity",
+                defaults: new { controller = "identity" },
+                constraints: null,
+                handler: new AuthenticationHandler(authentication, config)
+            );
+        }
+
+        private static AuthenticationConfiguration CreateAuthenticationConfiguration()
+        {
+            var authentication = new AuthenticationConfiguration
+            {
+                InheritHostClientIdentity = true,
+                DefaultAuthenticationScheme = "Basic"
+            };
+
+            authentication.AddBasicAuthentication((username, password) 
+                => UserCredentials.Validate(username, password));
+
+            return authentication;
+        }
+    }
+}
