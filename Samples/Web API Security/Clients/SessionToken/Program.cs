daef3ac1b156e5204710077b341a12cef363ce71
Dominick Baier <dbaier@leastprivilege.com>
2013-01-17 22:16:32 +0100

    updated session token client sample

--- Samples/Web API Security/Clients/SessionToken/Program.cs
+++ Samples/Web API Security/Clients/SessionToken/Program.cs
@@ -1,10 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using Thinktecture.IdentityModel.Extensions;
 using Thinktecture.Samples;
 
@@ -17,11 +13,8 @@ namespace SessionToken
 
         static void Main(string[] args)
         {
-            while (true)
-            {
-                RequestSessionToken();
-                Console.ReadLine();
-            }
+            var sessionToken = RequestSessionToken();
+            CallService(sessionToken);
         }
 
         private static string RequestSessionToken()
@@ -37,11 +30,39 @@ namespace SessionToken
             var tokenResponse = response.Content.ReadAsStringAsync().Result;
             var json = JObject.Parse(tokenResponse);
             var token = json["access_token"].ToString();
-            
+            var expiresIn = int.Parse(json["expires_in"].ToString());
+            var expiration = DateTime.UtcNow.AddSeconds(expiresIn);
+
             "\nSession Token:".ConsoleRed();
             Console.WriteLine(json.ToString());
 
+            "\nExpiration:".ConsoleRed();
+            Console.WriteLine(expiration.ToLongDateString() + " " + expiration.ToLongTimeString());
+
             return token;
+        }
+
+        private static void CallService(string token)
+        {
+            var client = new HttpClient { BaseAddress = _baseAddress };
+
+            client.SetToken("Session", token);
+
+            while (true)
+            {
+                "Calling service.".ConsoleYellow();
+
+                Helper.Timer(() =>
+                {
+                    var response = client.GetAsync("identity").Result;
+                    response.EnsureSuccessStatusCode();
+
+                    var claims = response.Content.ReadAsAsync<ViewClaims>().Result;
+                    Helper.ShowConsole(claims);
+                });
+
+                Console.ReadLine();
+            }
         }
     }
 }
