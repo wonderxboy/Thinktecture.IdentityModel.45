e0f9023fd928b946cbca7e247988cae8f1ba5deb
Dominick Baier <dbaier@leastprivilege.com>
2013-04-26 07:31:39 +0200

    Added extension method for asymmetric JWT signature

--- Samples/Web API Security/Common/Security/AuthenticationConfigurationExtensionsForMsftJwt.cs
+++ Samples/Web API Security/Common/Security/AuthenticationConfigurationExtensionsForMsftJwt.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens;
 using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
 using System.ServiceModel.Security.Tokens;
 using Thinktecture.IdentityModel.Constants;
 using Thinktecture.IdentityModel.Tokens.Http;
@@ -29,61 +30,26 @@ namespace Thinktecture.Samples.Security
                 Options = AuthenticationOptions.ForAuthorizationHeader(JwtConstants.Bearer),
                 Scheme = AuthenticationScheme.SchemeOnly(JwtConstants.Bearer)
             });
+        }
 
+        public static void AddMsftJsonWebToken(this AuthenticationConfiguration configuration, string issuer, string audience, X509Certificate2 signingCertificate)
+        {
+            var validationParameters = new TokenValidationParameters()
+            {
+                AllowedAudience = audience,
+                SigningToken = new X509SecurityToken(signingCertificate),
+                ValidIssuer = issuer,
+                ValidateExpiration = true
+            };
 
-            ////var handler = new JWTSecurityTokenHandler();
+            var handler = new JWTSecurityTokenHandlerWrapper(validationParameters);
 
-            //var handlerConfiguration = new SecurityTokenHandlerConfiguration();
-            //handlerConfiguration.AudienceRestriction.AllowedAudienceUris.Add(new Uri(audience));
-
-
-            //var registry = new ValidatingIssuerNameRegistry(new IssuingAuthority(issuer));
-
-            //handlerConfiguration.IssuerNameRegistry = registry;
-
-            //var issuerResolver = new NamedKeyIssuerTokenResolver(
-            //    new Dictionary<string, List<SecurityKey>> 
-            //    {
-            //        { 
-            //            issuer, new List<SecurityKey> 
-            //            {
-            //                new InMemorySymmetricSecurityKey(Convert.FromBase64String(signingKey))
-            //            }
-            //        }
-            //    });
-            //handlerConfiguration.IssuerTokenResolver = issuerResolver;
-
-            //var handler = new JWTSecurityTokenHandler();
-            //handler.Configuration = handlerConfiguration;
-
-           
-
-            //TokenValidationParameters validationParameters = new TokenValidationParameters
-            //{
-            //    ClockSkewInSeconds = Convert.ToUInt32(base.Configuration.MaxClockSkew.TotalSeconds)
-            //};
-            //List<string> list = new List<string>();
-            //foreach (Uri uri in base.Configuration.AudienceRestriction.AllowedAudienceUris)
-            //{
-            //    list.Add(uri.OriginalString);
-            //}
-            //validationParameters.AllowedAudiences = list;
-            //validationParameters.SigningTokenResolver = base.Configuration.IssuerTokenResolver;
-            //validationParameters.X509CertificateValidator = base.Configuration.CertificateValidator;
-            //validationParameters.SaveBootstrapContext = base.Configuration.SaveBootstrapContext;
-            //validationParameters.IssuerNameRegistry = base.Configuration.IssuerNameRegistry;
-            //List<ClaimsIdentity> list2 = new List<ClaimsIdentity>(this.ValidateToken(jwt, validationParameters).Identities);
-            //return list2.AsReadOnly();
-
-
-
-            //var config = new SecurityTokenHandlerConfiguration();
-            
-
-            //var handler = new JsonWebTokenHandler();
-            //handler.Configuration = config;
-
-           
+            configuration.AddMapping(new AuthenticationOptionMapping
+            {
+                TokenHandler = new SecurityTokenHandlerCollection { handler },
+                Options = AuthenticationOptions.ForAuthorizationHeader(JwtConstants.Bearer),
+                Scheme = AuthenticationScheme.SchemeOnly(JwtConstants.Bearer)
+            });
         }
     }
 
