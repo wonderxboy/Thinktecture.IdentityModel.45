db9ec3004ebc534f11a8350a363731c8b49d728a
Brock Allen <brockallen@gmail.com>
2013-07-03 16:26:14 -0400

    add MVC attribute to prevent clickjacking

---
+++ Samples/MVC/MvcApplication/MvcApplication/Controllers/XFrameOptionsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Thinktecture.IdentityModel.Web.Mvc;
+
+namespace MvcApplication.Controllers
+{
+    public class XFrameOptionsController : Controller
+    {
+        //
+        // GET: /XFrameOptions/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [FrameOptions]
+        public ActionResult Deny()
+        {
+            return View();
+        }
+
+        [FrameOptions(FrameOptions.SameOrigin)]
+        public ActionResult SameOrigin()
+        {
+            return View();
+        }
+
+        [FrameOptions("http://localhost:23626")]
+        public ActionResult CustomOrigin1()
+        {
+            return View();
+        }
+        
+        [FrameOptions("http://foo.com")]
+        public ActionResult CustomOrigin2()
+        {
+            return View();
+        }
+
+    }
+}
