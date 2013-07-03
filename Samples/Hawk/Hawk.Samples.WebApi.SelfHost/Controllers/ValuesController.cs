a3b9ef717c23229f5712a1550f160be4f88414c9
Dominick Baier <dbaier@leastprivilege.com>
2013-07-03 17:40:10 +0200

    Added Hawk sample

---
+++ Samples/Hawk/Hawk.Samples.WebApi.SelfHost/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Hawk.Samples.WebApi.SelfHost.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            var response = Request.CreateResponse<string>(HttpStatusCode.OK, "Thanks for flying Hawk");
+            response.Headers.Add("X-Response-Header-To-Protect", "Swoop"); // Sensitive header to be protected
+
+            return response;
+        }
+    }
+}
