0208a9bf1d5b3c97f9221ae0568669427f3a5a91
Dominick Baier <dbaier@leastprivilege.com>
2013-07-08 21:08:29 +0200

    Added SimpleHttp sample

---
+++ Samples/Web API Security/Clients/SimpleHttpClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using Thinktecture.IdentityModel.Extensions;
+using Thinktecture.Samples;
+
+namespace SimpleHttpClient
+{
+    class Program
+    {
+        static Uri _baseAddress = new Uri(Constants.WebHostBaseAddress);
+        static string tokenType = "swt";
+
+        static void Main(string[] args)
+        {
+            var token = GetToken();
+
+            Console.ReadLine();
+        }
+
+        private static string GetToken()
+        {
+            "Requesting token".ConsoleYellow();
+
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(Constants.IdSrv.SimpleHttpEndpoint)
+            };
+
+            client.SetBasicAuthentication("bob", "abc!123");
+
+            var response = client.GetAsync("?realm=" + Constants.Realm + "&tokentype=" + tokenType).Result;
+            response.EnsureSuccessStatusCode();
+
+            var tokenResponse = response.Content.ReadAsStringAsync().Result;
+            var token = JObject.Parse(tokenResponse)["access_token"].ToString();
+
+            Console.WriteLine(token);
+
+            return token;
+        }
+    }
+}
